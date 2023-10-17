46ca35e8d764deb3e55140db7847714fd7a0f57d
ahmadakbar00 <ahmadhiz2000@gmail.com>
2023-10-18 03:22:47 +0700

    firt

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImportExportController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller(ImportExportController::class)->group(function(){
+    Route::get('import_export', 'importExport');
+    Route::post('import', 'import')->name('import');
+    Route::get('export', 'export')->name('export');
+});
